b601a9dc522256a2831eaa109f8186c420011524
Andrew Wolf <andrew.wolfeus@gmail.com>
2019-04-08 23:45:08 +0000

    finished

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,22 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization' : 'Bearer ' + ' 594842a5f05d3bd26b24e538c3be2b80e3b61f6e'
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err,JSON.parse(body));
+  });
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
