a2899b9c538e82a56fa36c9188f93f01f2214aa4
Andrew Wolf <andrew.wolfeus@gmail.com>
2019-04-10 23:03:22 +0000

    request installed, npm initialized

--- download_avatars.js
+++ download_avatars.js
@@ -1,8 +1,6 @@
 var request = require('request');
 var token = require('./secrets').GITHUB_TOKEN;
-
-console.log('Welcome to the GitHub Avatar Downloader!');
-
+// console.log('Welcome to the GitHub Avatar Downloader!');
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
@@ -11,20 +9,26 @@ function getRepoContributors(repoOwner, repoName, cb) {
       'Authorization' : 'Bearer ' + token
     }
   };
-
   request(options, function(err, res, body) {
-    cb(err, JSON.parse(body));
+    for (user in JSON.parse(body)) {
+      cb(err, JSON.parse(body)[user].avatar_url);
+    };
   });
 }
 function downloadImageByURL(url, filePath) {
-  // ...
+  var request = require('request')
+  var fs = require('fs')
+  request.get(url)
+    .on('error', function (err) {
+      throw err;
+    })
+    .on('response', function (response) {
+      //console.log('Response Status Code: ', response.statusCode);
+      //console.log('Content Type: ', response.headers['content-type'])
+    })
+    .pipe(fs.createWriteStream(filePath));
 }
-
+downloadImageByURL('INSERT URL HERE', './images/i.jpg');
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-
-  for(i = 0; i < result.length; i++) {
-    console.log("Result:", result[i].avatar_url);
-  }
+  console.log(result);
 });
-
