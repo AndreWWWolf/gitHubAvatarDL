58302ea321d61996e5172bbb59a79fb841b7758a
Andrew Wolf <andrew.wolfeus@gmail.com>
2019-04-11 00:30:04 +0000

    process argv completed

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,5 @@
 var request = require('request');
 var token = require('./secrets').GITHUB_TOKEN;
-// console.log('Welcome to the GitHub Avatar Downloader!');
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
@@ -9,6 +8,7 @@ function getRepoContributors(repoOwner, repoName, cb) {
       'Authorization' : 'Bearer ' + token
     }
   };
+  console.log(repoOwner, repoName);
   request(options, function(err, res, body) {
     for (user in JSON.parse(body)) {
       cb(err, JSON.parse(body)[user]);
@@ -23,13 +23,9 @@ function downloadImageByURL(url, filePath) {
       throw err;
     })
     .on('response', function (response) {
-      //console.log('Response Status Code: ', response.statusCode);
-      //console.log('Content Type: ', response.headers['content-type'])
     })
     .pipe(fs.createWriteStream(filePath));
 }
-// downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "images/kvirani.jpg")
-getRepoContributors("jquery", "jquery", function(err, result) {
-  // console.log(result.avatar_url);
+getRepoContributors(process.argv[2], process.argv[3], function(err, result) {
   downloadImageByURL(result.avatar_url, 'images/' + result.login + '.jpg');
 });
